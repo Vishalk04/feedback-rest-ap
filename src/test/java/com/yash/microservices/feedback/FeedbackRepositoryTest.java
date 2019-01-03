8a30cd7cdd6d4acd111c775030aba3277bd08062
YASH\vishal.kumkar <vishal.kumkar@yash.com>
2019-01-03 15:22:59 +0530

    test cases h2 db issue resolved

--- src/test/java/com/yash/microservices/feedback/FeedbackRepositoryTest.java
+++ src/test/java/com/yash/microservices/feedback/FeedbackRepositoryTest.java
@@ -1,20 +1,23 @@
 package com.yash.microservices.feedback;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace=Replace.NONE)
 public class FeedbackRepositoryTest {
 
     @Autowired
