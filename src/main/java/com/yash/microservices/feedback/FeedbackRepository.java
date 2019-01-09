5453ef0c2d9f5e473140587f64005a8451f1461c
YASH\vishal.kumkar <vishal.kumkar@yash.com>
2019-01-09 16:20:55 +0530

    code clean

--- src/main/java/com/yash/microservices/feedback/FeedbackRepository.java
+++ src/main/java/com/yash/microservices/feedback/FeedbackRepository.java
@@ -10,29 +10,22 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class FeedbackRepository {
+	
 	private long id = 1;
-
 	Map<Long, Feedback> feedbackData = new HashMap<>();
 
 	public Optional<Feedback> findById(Long id) {
-		// TODO Auto-generated method stub
 		return Optional.of(feedbackData.get(id));
 	}
 
 	public List<Feedback> findAllByOrderByIdDesc() {
-		// TODO Auto-generated method stub
-		
-		
 		List<Feedback> feedback = new ArrayList<>(feedbackData.values());
-		
 		return feedback;
 	}
 
 	public Feedback save(Feedback feedback) {
 		feedback.setId(id++);
-		feedback.setRating(3);
 		feedbackData.put(feedback.getId(), feedback);
-		feedback.setId(id++);
 		return feedback;
 	}
 
