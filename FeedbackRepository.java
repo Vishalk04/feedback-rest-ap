f929207e383ae8ca1edcb9c5dcdb333c2be03618
YASH\vishal.kumkar <vishal.kumkar@yash.com>
2019-01-11 12:43:53 +0530

    clean code - removed unused classes

--- FeedbackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yash.microservices.feedback;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface FeedbackRepository extends CrudRepository<Feedback, Long> {
-    List<Feedback> findAllByOrderByIdDesc();
-}
