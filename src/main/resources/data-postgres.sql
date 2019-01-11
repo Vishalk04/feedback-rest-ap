ad52c7883f46c21381dfe5c72780fe27750d1dcb
Vishalk04 <42270298+Vishalk04@users.noreply.github.com>
2019-01-11 17:57:18 +0530

    data.sql change

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,6 +1,6 @@
 delete from feedback_schema.feedback; 
 insert into  feedback_schema.feedback( id , source,  rating , comment , username ) 
-values (2, 'yash', 5, 'na', 'usr123');
+values (1, 'yash', 5, 'na', 'usr123');
 insert into  feedback_schema.feedback( id , source,  rating , comment , username ) 
-values (3, 'yash', 4, 'na', 'usr111');
+values (2, 'yash', 4, 'na', 'usr111');
 
