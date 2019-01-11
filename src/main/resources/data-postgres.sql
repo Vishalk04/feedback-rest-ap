8e3761cbd1352e13d5603c79b8c74aa5d02c19dc
YASH\vishal.kumkar <vishal.kumkar@yash.com>
2019-01-11 14:41:29 +0530

    database change for popstgres

---
+++ src/main/resources/data-postgres.sql
@@ -0,0 +1,6 @@
+delete from feedback_schema.feedback; 
+insert into  feedback_schema.feedback( id , source,  rating , comment , username ) 
+values (2, 'yash', 5, 'na', 'usr123');
+insert into  feedback_schema.feedback( id , source,  rating , comment , username ) 
+values (3, 'yash', 4, 'na', 'usr111');
+
